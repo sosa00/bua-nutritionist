2a07a24f78e9df64d45a54e66218cac84b3cc685
Daniel Sosa <danielsosa1400@gmail.com>
2025-11-10 21:51:18 -0600

    Update Astro configuration to include trailing slash option and adjust favicon path for dynamic base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sosa00.github.io',
-  base: '/bua-nutritionist',
+  base: '/bua-nutritionist/',
+  trailingSlash: 'ignore',
 });
